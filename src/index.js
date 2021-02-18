d1194eae8152173d866ff91c57d244fd8e8163fa
Manikandan <62287301+Manipandian@users.noreply.github.com>
2021-02-18 16:54:41 +0530

    Trail

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./component/app";
 
+// "homepage": "http://manipandian.github.io/showPics",
 
 ReactDom.render(<App/>, document.querySelector("#root"));
